6f6c2f473f037b1ed1afb62c85e1be708d8eb040
P94Vansh <vanshgambhirag@gmail.com>
2024-09-24 00:46:15 +0530

    deployment problem

--- app/api/Contact/route.js
+++ app/api/Contact/route.js
@@ -16,7 +16,7 @@ export async function POST(request) {
         return new Response(JSON.stringify({ success: true }), {
             status: 200,
             headers: {
-                'Access-Control-Allow-Origin': 'https://portfolio-one-sigma-26.vercel.app/',
+                'Access-Control-Allow-Origin': 'https://portfolio-as55nhta5-p94vanshs-projects.vercel.app/',
                 'Access-Control-Allow-Methods': 'POST, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Content-Type': 'application/json',
@@ -26,7 +26,7 @@ export async function POST(request) {
         return new Response(JSON.stringify({ success: false }), {
             status: 404,
             headers: {
-                'Access-Control-Allow-Origin': 'https://portfolio-one-sigma-26.vercel.app/',
+                'Access-Control-Allow-Origin': 'https://portfolio-as55nhta5-p94vanshs-projects.vercel.app/',
                 'Access-Control-Allow-Methods': 'POST, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Content-Type': 'application/json',
@@ -40,7 +40,7 @@ export async function OPTIONS() {
     return new Response(null, {
         status: 200,
         headers: {
-            'Access-Control-Allow-Origin': 'https://portfolio-one-sigma-26.vercel.app/',
+            'Access-Control-Allow-Origin': 'https://portfolio-as55nhta5-p94vanshs-projects.vercel.app/',
             'Access-Control-Allow-Methods': 'POST, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type',
         },
